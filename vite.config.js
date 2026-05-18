35a19aa0e976fdff5be96e62d3ac538964b1b283
Martin Marguerat <martin.marguerat@sdataway.com>
2026-05-18 16:04:35 +0200

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/orinayo/chordpro-pdf-online/",
+  base: "/chordpro-pdf-online/",
   plugins: [
     vue(),
   ],
